b34960a1b63567f97a51fbc8084ca1e76bfc1a8e
Dev-Abdalla-Magdy <eng.abdalla.magdy@outlook.com>
2024-02-22 00:18:00 +0200

    Commiting Side Bar & Icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 const linksContainer = document.querySelector(".links_container");
 const menuBarBtn = document.querySelector(".menu_bar");
+const xBtn = document.querySelector("#X_btn");
+const lang = document.querySelector("#lang");
 
 menuBarBtn.addEventListener("click", showMenu);
+xBtn.addEventListener("click", closeMenu);
 
 function showMenu() {
-  if (linksContainer.classList.contains("active")) {
-    linksContainer.classList.add("hide");
-    linksContainer.classList.remove("active");
-  } else {
-    linksContainer.classList.add("active");
-    linksContainer.classList.remove("hide");
-  }
+  linksContainer.classList.add("active");
+}
+function closeMenu() {
+  linksContainer.classList.remove("active");
 }
