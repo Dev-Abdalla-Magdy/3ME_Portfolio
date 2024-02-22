846d1d5c76c2e58e5b2b245f93503a0d59eda120
Dev-Abdalla-Magdy <eng.abdalla.magdy@outlook.com>
2024-02-22 16:32:05 +0200

    Commit Landing Section Images Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
 const linksContainer = document.querySelector(".links_container");
+const landing = document.querySelector(".landing");
 const menuBarBtn = document.querySelector(".menu_bar");
 const xBtn = document.querySelector("#X_btn");
 const lang = document.querySelector("#lang");
+const landingSlider = document.querySelector(".slider");
+const landingImages = [
+  {
+    name: "url(/assets/files/landing_1.jpg)",
+  },
+  {
+    name: "url(/assets/files/landing_2.jpg)",
+  },
+  {
+    name: "url(/assets/files/landing_3.jpg)",
+  },
+  {
+    name: "url(/assets/files/landing_4.jpg)",
+  },
+  {
+    name: "url(/assets/files/landing_5.jpg)",
+  },
+  {
+    name: "url(/assets/files/3ME.jpg)",
+  },
+];
 
 menuBarBtn.addEventListener("click", showMenu);
 xBtn.addEventListener("click", closeMenu);
@@ -20,3 +42,19 @@ function changeLang() {
     lang.textContent = "Ar";
   }
 }
+function slideImagseLanding() {
+  for (i in landingImages) {
+    let singleBar = document.createElement("span");
+    singleBar.className = "single_bar";
+    landingSlider.appendChild(singleBar);
+  }
+}
+
+function changeLandingImage() {
+  let rNum = Math.floor(Math.random() * landingImages.length - 0.5);
+  landing.style.backgroundImage = landingImages[rNum].name;
+}
+slideImagseLanding();
+setInterval(() => {
+  changeLandingImage();
+}, 5000);
